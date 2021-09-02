dab7425b86c1320fabc6185b83a7a62dfc8fa8a6
Estefane <estefanecortes@gmail.com>
2021-09-01 22:55:36 -0300

    atualizando depedencias e schema

---  schema.sql
+++  schema.sql
@@ -3,5 +3,5 @@ CREATE DATABASE newsletter;
 CREATE TABLE subscribers(
   id serial PRIMARY KEY,
   nome varchar(100) NOT NULL,
-  email varchar(200) NOT NULL
+  email varchar(200) UNIQUE NOT NULL
 );
