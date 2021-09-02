ecd0aa00b97dad2caf81cc48f22b3101552b3f93
Estefane <estefanecortes@gmail.com>
2021-09-01 22:56:16 -0300

    consertando connection

--- src/connection.js
+++ src/connection.js
@@ -1,14 +1,10 @@
-const knex = require('knex');({
+const knex = require('knex')({
     client: 'pg',
     connection: {
         host: 'localhost',
         user: 'postgres',
         password: 'postgres',
-        database: 'newsletter',
-        port: 8000,
-        ssl: {
-            rejectUnauthorized: false
-        }
+        database: 'newsletter'
     }
 })
 
