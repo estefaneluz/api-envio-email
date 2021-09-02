fc56f0e74a1aec5ed3b5a7d0770824ad73ffe95f
Estefane <estefanecortes@gmail.com>
2021-09-01 22:56:45 -0300

    funcao subscriber criada

--- src/controllers/subscribers/index.js
+++ src/controllers/subscribers/index.js
@@ -1,7 +1,32 @@
 const connection = require('../../connection');
 
-const subscribe = async () => {
+const subscribe = async (req, res) => {
+    const { nome, email } = req.body;
+    if(!nome) return res.status(400).json("O nome precisa ser informado.");
+    if(!email) return res.status(400).json("O e-mail precisa ser informado.");
 
+    try {
+        const subscribersCount = await connection('subscribers').where({email}).first();
+
+        if(subscribersCount){
+            return res.status(400).json("O e-mail já existe.");
+        }
+
+        const subscriber = {
+            nome,
+            email
+        }
+
+        const sub = await connection('subscribers').insert(subscriber);
+        if(sub.rowCount === 0) {
+            return res.status(400).json("Não foi possível fazer a inscrição.");
+        }
+
+        return res.status(200).json("A inscrição foi feita com sucesso!");
+
+    } catch(error){
+        res.status(400).json(error.message);
+    }
 }
 
 module.exports = { subscribe }
